e35f9b37a955efcbd5a7678a1e04146c16681d02
Dhrubo Raj Roy <Dhruborajroy3@gmail.com>
2022-06-21 10:05:34 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
 <?php 
-function emailBody($otp){
-    return $body='<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-	  <div style="margin:50px auto;width:70%;padding:20px 0">
-	    <div style="border-bottom:1px solid #eee">
-	      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Lalmonirhat Science & Literature Club</a>
-	    </div>
-	    <p style="font-size:1.1em">Hi, Dhrubo</p>
-	    <p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
-
-	    <div style="display:inline-flex;">
-	    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">'.$otp.'</h2>
-	    <p style="margin-left:10px">OR</p>
-	    <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600"><h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;margin-left:10px">Verify Email</h2></a>
-	    </div>
-
-	    <p style="font-size:0.9em;">Regards,<br />Your Brand</p>
-	    <hr style="border:none;border-top:1px solid #eee" />
-	    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-	      <p>Lalmonirhat Science & Literature Club</p>
-	      <p>Lalmonirhat</p>
-	    </div>
-	  </div>
-	</div>';
-}
-$html=emailBody(555);
 require("./smtp/class.phpmailer.php");
 function send_email($email,$html,$subject){
 	$mail=new PHPMailer(true);
@@ -55,3 +30,17 @@ function send_email($email,$html,$subject){
 	}
 }
 echo send_email("Dhruborajroy3@gmail.com",$html,"otp1");
+
+
+
+function send_email_using_tamplate(){
+	$tamplate= "./email.php";
+	$file_content=file_get_contents($tamplate);
+	$array=array(
+		"{YOUR_NAME}"=>"DHRUBO 1",
+		"{OTP_NUMBER}"=>rand(),
+	);
+	$keys = array_keys($array);
+	$values = array_values($array);
+	return str_replace($keys, $values, $file_content);
+}
